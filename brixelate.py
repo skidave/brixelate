019712755afa15826c97e15b142183b4e79a4930
skidave <d.r.f.mathias@gmail.com>
2018-05-01 16:15:06 +0100

    Pulled out brixelate functionality to separate class

--- brixelate.py
+++ brixelate.py
@@ -48,10 +48,7 @@ class BrixelPanel(Panel):
 	def draw(self, context):
 		scene = context.scene
 		settings = scene.my_settings
-		bricks = scene.model_data.bricks
-		opt_bricks = scene.model_data.opt_bricks
-		brick_count = scene.model_data.brick_count
-		opt_brick_count = scene.model_data.opt_brick_count
+		brick_count = scene.lego_data.brick_count
 
 		layout = self.layout
 		col = layout.column(align=True)
@@ -129,7 +126,7 @@ class BrixelPanel(Panel):
 		row = box.row()
 		row.operator("tool.simple_brixelate", text="Go", icon="FILE_TICK")
 
-		if scene.model_data.name is not None:
+		if len(scene.objects) > 0:
 			layout.separator()
 			col = layout.column(align=True)
 			box = col.box()
@@ -138,16 +135,11 @@ class BrixelPanel(Panel):
 			row.label(text="Object", icon="VIEW3D")
 			row.prop(settings, "show_hide_model", text=("Visible" if settings.show_hide_model else "Hidden"),
 					 toggle=True)
-			if brick_count > 0:
-				row = box.row()
-				row.label(text="Basic", icon="GROUP_VERTEX")
-				row.prop(settings, "show_hide_lego", text=("Visible" if settings.show_hide_lego else "Hidden"),
-						 toggle=True)
-			if opt_brick_count > 0:
-				row = box.row()
-				row.label(text="Layout", icon="NLA")
-				row.prop(settings, "show_hide_optimised",
-						 text=("Visible" if settings.show_hide_optimised else "Hidden"), toggle=True)
+		if brick_count > 0:
+			row = box.row()
+			row.label(text="LEGO", icon="GROUP_VERTEX")
+			row.prop(settings, "show_hide_lego", text=("Visible" if settings.show_hide_lego else "Hidden"),
+					 toggle=True)
 
 		layout.separator()
 		layout.operator("tool.reset_brixelate", text="Reset", icon="FILE_REFRESH")
@@ -158,10 +150,8 @@ class BrixelPanel(Panel):
 			box = col.box()
 			box.label("Info", icon="INFO")
 			row = box.row()
-			row.label("Basic Brick Count:  %d" % brick_count)
-			if opt_brick_count > 0:
-				row = box.row()
-				row.label("Layout Brick Count:  %d" % opt_brick_count)
+			row.label("Brick Count:  {0}".format(brick_count))
+
 
 # end draw
 
@@ -180,19 +170,25 @@ class simpleBrixelate(Operator):
 			return True
 
 	def execute(self, context):
+		Brixelate = context.scene.Brixelate
 		object_selected = context.selected_objects[0]
-		self.brixelate(context.scene, object_selected)
+		use_shell_as_bounds = context.scene.my_settings.use_shell_as_bounds
+		bricks_to_use, _ = context.scene.lego_data.listOfBricksToUse()
+
+		Brixelate.brixelate(context.scene, object_selected, use_shell_as_bounds, bricks_to_use)
+		self.report({"INFO"}, "Brixelate finished")
 		return {'FINISHED'}
 
 	def invoke(self, context, event):
 		return self.execute(context)
 
-	def brixelate(self, scene, object_selected):
+
+# end simpleBrixelate
+
+class brixelateFunctions():
+	def brixelate(self, scene, object_selected, use_shell_as_bounds, bricks_to_use):
 
 		lego = scene.lego_data
-		addPlateAtPoint = lego.addPlateAtPoint
-		createInitialPlate = lego.createInitialPlate
-		copyPlatesToPoints = lego.copyPlatesToPoints
 
 		xyz_bricks, start_point = self.brickBounds(scene, object_selected)
 		xbricks = math.ceil(xyz_bricks[0] / 2)
@@ -209,7 +205,6 @@ class simpleBrixelate(Operator):
 		getVertices = meshCheck.getVertices
 		getEdges = meshCheck.getEdges
 		rayInside = meshCheck.rayInside
-		use_shell_as_bounds = scene.my_settings.use_shell_as_bounds
 
 		object_selected.select = False
 
@@ -229,10 +224,10 @@ class simpleBrixelate(Operator):
 						if centreIntersect or sum(edgeIntersects) > 0:
 							bricks_array[z + zbricks, y + ybricks, x + xbricks] = 1
 		end_time = time.time()
-		self.report({"PROPERTY"}, "Testing done in %5.3f seconds" % (end_time - start_time))
+		testing_time = (end_time - start_time)
 
-		lego_volume, used_bricks_dict = self.brickPacking(scene, bricks_array, start_point)
-		print(lego_volume)
+		lego_volume, used_bricks_dict, brick_count = self.brickPacking(scene, bricks_array, start_point, bricks_to_use)
+		# print(lego_volume)
 		print(used_bricks_dict)
 
 		# bm = self.bmesh_copy_from_object(object_selected, apply_modifiers=True)
@@ -241,24 +236,28 @@ class simpleBrixelate(Operator):
 		bmesh.ops.triangulate(bm, faces=bm.faces)
 		object_volume = bm.calc_volume()
 
-		print(object_volume)
+		# print(object_volume)
 		# volume_difference = abs(object_volume - lego_volume)
 		volume_percent = (lego_volume / object_volume)
 
-		self.report({"INFO"}, "{:.2%}  LEGO -> Object Volume".format(volume_percent))
+		volume_string = "{:.2%}  LEGO -> Object Volume".format(volume_percent)
 
 		# TODO parent bricks to the selected object
 
 		object_selected.select = True
-		self.report({"INFO"}, "Brixelate finished")
-		return {'FINISHED'}
+
+		scene.my_settings.show_hide_model = True
+		scene.my_settings.show_hide_lego = True
+
+		#name, vol, brick_count,
+		# output_data =
+
+		return None
 
 	def brickBounds(self, scene, object_selected):
-		model_data = scene.model_data
-		model_data.name = object_selected.name
 		lego = scene.lego_data
 
-		bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)
+		bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
 		vertices = [object_selected.matrix_world * Vector(corner) for corner in object_selected.bound_box]
 		x_vec = vertices[4] - vertices[0]
 		y_vec = vertices[3] - vertices[0]
@@ -268,7 +267,7 @@ class simpleBrixelate(Operator):
 		y_dim = round(y_vec.length, 4)
 		z_dim = round(z_vec.length, 4)
 
-		origin = object_selected.matrix_world.to_translation()
+		# origin = object_selected.matrix_world.to_translation()
 
 		start_point = vertices[0] + (x_vec / 2) + (y_vec / 2) + (z_vec / 2)
 
@@ -281,27 +280,16 @@ class simpleBrixelate(Operator):
 		scene.my_settings.show_hide_model = True
 		scene.my_settings.show_hide_lego = True
 
-		self.report({'PROPERTY'}, "LEGO Dimensions  X:{0} Y:{1} Z:{2}".format(x_brick, y_brick, z_brick))
+		lego_dimensions_string = "LEGO Dimensions  X:{0} Y:{1} Z:{2}".format(x_brick, y_brick, z_brick)
 
-		total_size = x_brick + y_brick + z_brick
-		if total_size < 10:
-			self.report({'WARNING'}, "Model is very small, consider scaling it larger.")
 		return xyz_brick, start_point
 
-	def brickPacking(self, scene, bricks_array, start_point):
+	def brickPacking(self, scene, bricks_array, start_point, directional_list_of_bricks):
 		start_time = time.time()
 		lego_data = scene.lego_data
 
 		bricks = bricks_array
 		opt_bricks = copy.copy(bricks_array) * -1.0
-
-		list_of_pieces = lego_data.bricksToUseInLayout()
-		directional_list_of_pieces = []
-		for piece in list_of_pieces:
-			directional_list_of_pieces.append(piece)
-			if piece[0] != piece[1]:
-				piece_alt_dir = [piece[1], piece[0], piece[2]]
-				directional_list_of_pieces.append(piece_alt_dir)
 
 		w = lego_data.plate_w
 		d = lego_data.plate_d
@@ -320,7 +308,7 @@ class simpleBrixelate(Operator):
 			for y in range(y_array):
 				for x in range(x_array):
 					if bricks[z, y, x] == 1 and opt_bricks[z, y, x] == -1:
-						for piece in directional_list_of_pieces:
+						for piece in directional_list_of_bricks:
 
 							count = 0
 							next_piece = False
@@ -351,7 +339,7 @@ class simpleBrixelate(Operator):
 											break
 
 							if count == max_count:
-								used_bricks.append(lego_data.pieceName(piece))
+								used_bricks.append(lego_data.brickName(piece))
 
 								for p in p_list:
 									opt_bricks[p[0], p[1], p[2]] = brick_num
@@ -377,10 +365,13 @@ class simpleBrixelate(Operator):
 		scene.my_settings.show_hide_lego = False
 		scene.my_settings.show_hide_optimised = True
 
-		end_time = time.time()
-		self.report({"PROPERTY"}, "Packing done in %5.3f seconds" % (end_time - start_time))
+		brick_count = brick_num - 1
+		lego_data.brick_count = brick_count
 
-		return lego_volume, used_bricks_dict
+		end_time = time.time()
+		packing_time = (end_time - start_time)
+
+		return lego_volume, used_bricks_dict, brick_count
 
 	def bmesh_copy_from_object(obj, transform=True, triangulate=True, apply_modifiers=False):
 		"""
@@ -388,7 +379,7 @@ class simpleBrixelate(Operator):
 		"""
 
 		# assert (obj.type == 'MESH')
-		print(obj.type)
+		# print(obj.type)
 
 		if apply_modifiers and obj.modifiers:
 			import bpy
@@ -415,8 +406,6 @@ class simpleBrixelate(Operator):
 		return bm
 
 
-# end simpleBrixelate
-
 class resetBrixelate(Operator):
 	'''Removes all LEGO bricks'''
 	bl_idname = "tool.reset_brixelate"
@@ -425,24 +414,23 @@ class resetBrixelate(Operator):
 	@classmethod
 	def poll(self, context):
 		scene = context.scene
-		if len(scene.objects) > 1:
+		if len(scene.objects) > 0:
 			return True
 
 	def execute(self, context):
 		start_time = time.time()
 		scene = context.scene
-		model_name = scene.model_data.name
 
 		objs = bpy.data.objects
 		for ob in scene.objects:
 			ob.hide = False
-			if ob.name.startswith('Brick'):
+			if ob.name.startswith('Brick '):
 				objs.remove(ob, True)
 
 		scene.my_settings.show_hide_model = True
 		scene.my_settings.show_hide_lego = True
+		scene.lego_data.brick_count = 0
 
-		# bpy.ops.tool.bounding_box_size()
 		end_time = time.time()
 		self.report({"INFO"}, "Reset finished in %5.3f seconds" % (end_time - start_time))
 
@@ -463,6 +451,8 @@ class legoData():
 	x_brick = None
 	y_brick = None
 	z_brick = None
+
+	brick_count = 0
 
 	list_of_1plates = [
 		[8, 1, 1],
@@ -498,147 +488,25 @@ class legoData():
 		[3, 2, 3],
 		[2, 2, 3]]
 
-	def pieceName(self, piece):
-		if piece[2] == 3:
+	def brickName(self, brick):
+		if brick[2] == 3:
 			type = 'Brick'
 		else:
 			type = 'Plate'
 
-		if piece[0] > piece[1]:
-			first = piece[0]
-			second = piece[1]
+		if brick[0] > brick[1]:
+			first = brick[0]
+			second = brick[1]
 		else:
-			first = piece[1]
-			second = piece[0]
+			first = brick[1]
+			second = brick[0]
 
-		pieceName = '{0}x{1} {2}'.format(first, second, type)
+		name = '{0}x{1} {2}'.format(first, second, type)
 
-		return pieceName
-
-	# function to create a 1x1 plate at a point
-	def addPlateAtPoint(self, point, indices):
-		scene = bpy.context.scene
-		z, y, x = indices
-
-		Vertices = \
-			[
-				Vector((0, 0, 0)),
-				Vector((0, self.plate_d, 0)),
-				Vector((self.plate_w, self.plate_d, 0)),
-				Vector((self.plate_w, 0, 0)),
-				Vector((0, 0, self.plate_h)),
-				Vector((0, self.plate_d, self.plate_h)),
-				Vector((self.plate_w, self.plate_d, self.plate_h)),
-				Vector((self.plate_w, 0, self.plate_h)),
-			]
-
-		Faces = \
-			[
-				(0, 1, 2, 3),
-				(5, 4, 7, 6),
-				(0, 4, 5, 1),
-				(2, 1, 5, 6),
-				(2, 6, 7, 3),
-				(3, 7, 4, 0)
-			]
-		name_string = "1x1 position: " + str(x) + ',' + str(y) + ',' + str(z)
-		newMesh = bpy.data.meshes.new(name_string)
-		newMesh.from_pydata(Vertices, [], Faces)
-		newMesh.update()
-		new_plate = bpy.data.objects.new(name_string, newMesh)
-
-		# Change brick colour
-		mat_name_string = "Colour " + name_string
-		colour = bpy.data.materials.new(name=mat_name_string)
-		new_plate.data.materials.append(colour)
-		new_plate.data.materials[0].diffuse_color = (random.uniform(0.2, 1), 0, 0)
-
-		bpy.context.scene.objects.link(new_plate)
-
-		new_plate.select = True
-		bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
-		new_plate.select = False
-		new_plate.location = point
-
-	def createInitialPlate(self):
-		Vertices = \
-			[
-				Vector((0, 0, 0)),
-				Vector((0, self.plate_d, 0)),
-				Vector((self.plate_w, self.plate_d, 0)),
-				Vector((self.plate_w, 0, 0)),
-				Vector((0, 0, self.plate_h)),
-				Vector((0, self.plate_d, self.plate_h)),
-				Vector((self.plate_w, self.plate_d, self.plate_h)),
-				Vector((self.plate_w, 0, self.plate_h)),
-			]
-
-		Faces = \
-			[
-				(0, 1, 2, 3),
-				(5, 4, 7, 6),
-				(0, 4, 5, 1),
-				(2, 1, 5, 6),
-				(2, 6, 7, 3),
-				(3, 7, 4, 0)
-			]
-		name_string = "initial_plate"
-		newMesh = bpy.data.meshes.new(name_string)
-		newMesh.from_pydata(Vertices, [], Faces)
-		newMesh.update()
-		init_plate = bpy.data.objects.new(name_string, newMesh)
-		bpy.context.scene.objects.link(init_plate)
-		init_plate.select = True
-		bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
-		init_plate.select = False
-		init_plate.location = Vector((0, 0, 0))
-
-	def copyPlatesToPoints(self, start_point, bricks_array):
-		self.createInitialPlate()
-		z_array, y_array, x_array = bricks_array.shape[0], bricks_array.shape[1], bricks_array.shape[2]
-		x_offset = (x_array - 1) / 2
-		y_offset = (y_array - 1) / 2
-		z_offset = (z_array - 1) / 2
-
-		w = self.plate_w
-		d = self.plate_d
-		h = self.plate_h
-
-		new_plates = []
-		initial_plate = bpy.data.objects["initial_plate"]
-		start_time = time.time()
-		for x in range(x_array):
-			for y in range(y_array):
-				for z in range(z_array):
-					if bricks_array[z, y, x] == 1:
-						translation = Vector(((x - x_offset) * w, (y - y_offset) * d, (z - z_offset) * h)) + start_point
-						copy = initial_plate.copy()
-						copy.location = translation
-						copy.data = copy.data.copy()  # also duplicate mesh, remove for linked duplicate
-						copy.name = "1x1 position: " + str(x) + ',' + str(y) + ',' + str(z)
-						new_plates.append(copy)
-
-		end_time = time.time()
-		print("Copying: Translating done in %5.3f seconds" % (end_time - start_time))
-
-		start_time = time.time()
-		for plate in new_plates:
-			bpy.context.scene.objects.link(plate)
-			self.randomiseColour(plate)
-		end_time = time.time()
-		print("Copying: Linking done in %5.3f seconds" % (end_time - start_time))
-
-		bpy.data.objects.remove(initial_plate, True)
-
-	def randomiseColour(self, object):
-		mat_name_string = "Colour " + object.name
-		colour = bpy.data.materials.new(name=mat_name_string)
-		object.data.materials.append(colour)
-		object.data.materials[0].diffuse_color = (random.uniform(0.2, 1), 0, 0)
+		return name
 
 	# function to create a brick with width, depth and height at a point
 	def addNewBrickAtPoint(self, point, width, depth, height, number):
-
 		w = self.plate_w
 		d = self.plate_d
 		h = self.plate_h
@@ -680,7 +548,13 @@ class legoData():
 		new_brick.select = False
 		new_brick.location = point
 
-	def bricksToUseInLayout(self):
+	def randomiseColour(self, object):
+		mat_name_string = "Colour " + object.name
+		colour = bpy.data.materials.new(name=mat_name_string)
+		object.data.materials.append(colour)
+		object.data.materials[0].diffuse_color = (random.uniform(0.2, 1), 0, 0)
+
+	def availableBricks(self):
 		settings = bpy.context.scene.my_settings
 		toUse = []
 		add = toUse.append
@@ -708,17 +582,18 @@ class legoData():
 		toUse_ = np.array(toUse)
 		return toUse
 
+	def listOfBricksToUse(self):
+		list_of_bricks = self.availableBricks()
+		directional_list_of_bricks = []
+		brick_names = []
 
-class modelData():
-	name = None
-	location = None
-	rotation = None
-	scale = None
-	start_point = []
-	bricks = np.array([[[0]]])  # name, corners, edges, faces
-	brick_count = 0
-	opt_bricks = np.array([[[0]]])
-	opt_brick_count = 0
+		for brick in list_of_bricks:
+			directional_list_of_bricks.append(brick)
+			brick_names.append(self.brickName(brick))
+			if brick[0] != brick[1]:
+				piece_alt_dir = [brick[1], brick[0], brick[2]]
+				directional_list_of_bricks.append(piece_alt_dir)
+		return directional_list_of_bricks, brick_names
 
 
 class meshCheck():
@@ -875,32 +750,21 @@ class meshCheck():
 
 def showHideModel(self, context):
 	scene = context.scene
-	model_name = scene.model_data.name
-	model = scene.objects[model_name]
-	model.hide = not scene.my_settings.show_hide_model
+
+	string = "Brick "
+	for ob in scene.objects:
+		if ob.name.startswith(string) is False:
+			ob.hide = not scene.my_settings.show_hide_model
 	return None
 
 
 def showHideLEGO(self, context):
 	scene = context.scene
-	model_name = scene.model_data.name
-
-	string = "1x1 position: "
-	for ob in scene.objects:
-		if ob.name.startswith(string) and ob.name != model_name:
-			ob.hide = not scene.my_settings.show_hide_lego
-
-	return None
-
-
-def showHideOpt(self, context):
-	scene = context.scene
-	model_name = scene.model_data.name
 
 	string = "Brick "
 	for ob in scene.objects:
-		if ob.name.startswith(string) and ob.name != model_name:
-			ob.hide = not scene.my_settings.show_hide_optimised
+		if ob.name.startswith(string):
+			ob.hide = not scene.my_settings.show_hide_lego
 
 	return None
 
@@ -950,13 +814,6 @@ class MySettings(PropertyGroup):
 		update=showHideLEGO
 	)
 
-	show_hide_optimised = BoolProperty(
-		name="Show/Hide Optimised",
-		description="Toggles Brick layout visibility",
-		default=True,
-		update=showHideOpt
-	)
-
 	brick_selection = BoolProperty(name="Expand Brick Selection", default=False)
 
 	all_plates = BoolProperty(name="Toggles All Plates", default=True, update=allPlates)
@@ -979,8 +836,8 @@ def register():
 	for c in classes:
 		register_class(c)
 
+	bpy.types.Scene.Brixelate = brixelateFunctions()
 	bpy.types.Scene.lego_data = legoData()
-	bpy.types.Scene.model_data = modelData()
 	bpy.types.Scene.mesh_check = meshCheck()
 	bpy.types.Scene.my_settings = PointerProperty(type=MySettings)
 
@@ -991,8 +848,8 @@ def unregister():
 	for c in classes:
 		unregister_class(c)
 
+	del bpy.types.Scene.Brixelate
 	del bpy.types.Scene.lego_data
-	del bpy.types.Scene.model_data
 	del bpy.types.Scene.mesh_check
 	del bpy.types.Scene.my_settings
 
